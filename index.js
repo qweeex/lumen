897a8041ede7e3799bf40a1feb470b3979422a91
Arkady Pugachev <qweeex@yandex.com>
2020-04-11 13:39:03 +0400

    red

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function check() {
 function btn() {
 
 
-    let x1 = $('#x').val();;
-    let y1 = $('#y').val();;
-    let h1 = $('#h').val();;
-    let n1 = $('#n').val();;
-    let l1 = $('#l').val();;
+    let x1 = $('#x').val();
+    let y1 = $('#y').val();
+    let h1 = $('#h').val();
+    let n1 = $('#n').val();
+    let l1 = $('#l').val();
 
     let x = Number(x1);
     let y = Number(y1);
@@ -34,27 +34,6 @@ function btn() {
     $('#desc-w').html(x);
     $('#desc-h').html(y);
 
-
-    function higtlight(square, height){
-        let k;
-
-        if (height <= 2.7){
-            k = 1;
-        } 
-        if (height >= 2.8 && height <= 3){
-            k = 1.2;
-        }
-        if (height >= 3.1 && height <= 3.5){
-            k = 1.5;
-        }
-        if (height > 3.6) {
-            k = 2;
-        }
-        
-        /// 300 это нормативное значение освещенности
-        return 300 * square * k;
-    }
-
     s = x * y;
     $('#res-s').html(s); //площадь
     $('#res-o').html(higtlight(s, h)); //Необходимая освещенность помещения
@@ -66,7 +45,7 @@ function btn() {
     // ________________________________________________________//
 
     $('#kolvovat').html(Math.round((higtlight(s, h) / 100) / l)); //для светодиодных  для 1 лампы
-    $('#vats').html(Math.round(higtlight(s, h) / 100)); //для светодиодных  для всех ламп
+    $('#vats').html(Math.round((higtlight(s, h) / 100) / l) * l); //для светодиодных  для всех ламп
 
     // ________________________________________________________//
 
@@ -75,7 +54,7 @@ function btn() {
     // ________________________________________________________//
 
     $('#kolvovat2').html(Math.round((higtlight(s, h) / 40) / l)); //для Люминесцентные  для 1 лампы
-    $('#vats2').html(Math.round(higtlight(s, h) / 40)); //для Люминесцентные  для всех ламп
+    $('#vats2').html(Math.round((higtlight(s, h) / 40) / l) * l); //для Люминесцентные  для всех ламп
 
     // ________________________________________________________//
 
@@ -83,7 +62,7 @@ function btn() {
     // ________________________________________________________//
 
     $('#kolvovat3').html(Math.round((higtlight(s, h) / 10) / l)); //для накаливания   для 1 лампы
-    $('#vats3').html(Math.round(higtlight(s, h) / 10)); //для накаливания   для всех ламп
+    $('#vats3').html(Math.round((higtlight(s, h) / 10) / l) * l); //для накаливания   для всех ламп
 
     // ________________________________________________________//
 
@@ -94,4 +73,24 @@ function btn() {
 
 
 
+}
+
+function higtlight(square, height){
+    let k;
+
+    if (height <= 2.7){
+        k = 1;
+    } 
+    if (height >= 2.8 && height <= 3){
+        k = 1.2;
+    }
+    if (height >= 3.1 && height <= 3.5){
+        k = 1.5;
+    }
+    if (height > 3.6) {
+        k = 2;
+    }
+    
+    /// 300 это нормативное значение освещенности
+    return 300 * square * k;
 }
